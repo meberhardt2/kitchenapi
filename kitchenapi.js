a532a1e1b6f347c74373ac5f12295881e2516a90
meberhardt2 <matthew.eberhardt@gmail.com>
2019-03-13 13:47:34 -0500

    switched express over to https

--- kitchenapi.js
+++ kitchenapi.js
@@ -4,19 +4,25 @@ const bookmark = require('./routes/bookmark');
 const tags = require('./routes/tags');
 const recipe = require('./routes/recipe');
 const search = require('./routes/search');
+const fs = require('fs');
+const https = require('https');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// app.delete('/api/v1/todos/:id', (req, res) => {
-// req.params.id
 
-app.listen(2001, () => {
-	console.log("Server running on port 2001");
-});
+const sslOptions = {
+    key: fs.readFileSync("/var/www/certs/kitchen.local.key"),
+    cert: fs.readFileSync("/var/www/certs/kitchen.local.crt")
+};
 
 
+//app.listen(2001, () => {
+//	console.log("Server running on port 2001");
+//});
+
+https.createServer(sslOptions, app).listen(2001);
 
 
 const sqlite3 = require('sqlite3').verbose();

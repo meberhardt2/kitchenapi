49525ec641762a0b577e57baee2ec64b4c219603
meberhardt2 <matthew.eberhardt@gmail.com>
2019-03-14 19:07:33 -0500

    image upload tweaks

--- routes/camera.js
+++ routes/camera.js
@@ -4,8 +4,9 @@ const camera = (app,DB,fs) => {
 	/********************************************/
 	app.post('/api/camera', (request, response) => {
 		let post = request.body;
-		let bitmap = new Buffer(post.image, 'base64');
-		fs.writeFileSync("/var/www/kitchenapi/uploads/for_ocr.png", bitmap);
+		let bitmap = Buffer.from(post.image, 'base64');
+		
+		fs.writeFileSync("/var/www/kitchen/uploads/for_ocr.png", bitmap);
 
 		response.json({'status': 'ok'});
 	});

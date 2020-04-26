6cbea6fde5fb5176a7f643778177ba2769ab2fe5
meberhardt2 <matthew.eberhardt@gmail.com>
2020-04-26 12:31:42 -0500

    no longer using reverse proxy. update to api to use 80 and 443 with certbot

--- kitchenapi.js
+++ kitchenapi.js
@@ -7,8 +7,8 @@ const search = require('./routes/search');
 const camera = require('./routes/camera');
 const upload = require('./routes/upload');
 const fs = require('fs');
-//const https = require('https');
-const http = require('http');
+const https = require('https');
+//const http = require('http');
 const tesseract = require('node-tesseract');
 const multer = require('multer');
 
@@ -20,17 +20,17 @@ app.use(express.json({limit: '50mb'}));
 
 ///etc/letsencrypt/live/eberhardt.cloud
 
-/*
+
 const sslOptions = {
     key: fs.readFileSync("/etc/letsencrypt/live/eberhardt.cloud/privkey.pem"),
     cert: fs.readFileSync("/etc/letsencrypt/live/eberhardt.cloud/fullchain.pem")
 };
-*/
+
 
 const uploadMulter = multer({ dest: '/var/www/kitchenExpress/uploads/' });
 
-//https.createServer(sslOptions, app).listen(2001);
-http.createServer(app).listen(2001);
+https.createServer(sslOptions, app).listen(2004);
+//http.createServer(app).listen(2003);
 //http.timeout = 10000;
 
 //node-sqlite3 runs asynchronous, which leads to promise mess of chaining. better-sqlite3 runs synchronously
